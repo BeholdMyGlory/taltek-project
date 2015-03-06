c5efb83d41f0320bcd0fcfb1c19833891f93a2b6
BeholdMyGlory <behold@behold.se>
2015-03-06 14:53:28 +0100

    Updated Tornado skeleton to reflect the discussed API

--- main.py
+++ main.py
@@ -11,9 +11,11 @@ Options:
 """
 
 import datetime
+import logging
 import uuid
 
 from docopt import docopt
+from lxml import etree
 import tornado.ioloop
 import tornado.gen
 import tornado.web
@@ -23,42 +25,69 @@ class XMLHandler(tornado.web.RequestHandler):
     def prepare(self):
         self.set_header("Content-Type", "application/xml")
 
+    def write_xml(self, tagname="response", **attributes):
+        self.write(etree.tostring(etree.Element(tagname, **attributes)))
+
 # new instances of the request handlers are created on every request,
 # so changes to member variables won't be seen across requests
-class HelloWorld(XMLHandler):
+class DialogHandler(XMLHandler):
     def get(self):
         token = uuid.uuid1().hex
         self.render("dialog.xml", token=token)
 
 
-class FindMatchHandler(XMLHandler):
+class WaitForGameHandler(XMLHandler):
     @tornado.gen.coroutine
     def get(self):
-        print(self.get_argument("token"))
+        logger.debug("WaitForGame: %s", self.request.query)
         yield tornado.gen.sleep(9)
-        self.write("<response>yes</response>")
+        self.write_xml(ready="true")
 
-placed_ships = {}
-ships = [("battleship", 4), ("submarine", 3), ("destroyer", 2)]
-
-class NextShipHandler(XMLHandler):
+class PlaceShipHandler(XMLHandler):
     def get(self):
-        token = self.get_argument('token')
-        placed_ships[token] = placed_ships.get(token, -1) + 1
-        ship, length = ships[placed_ships[token]]
-        self.write('<response ship="{}" length="{}"/>'.format(ship, length))
+        logger.debug("GET-PlaceShip: %s", self.request.query)
+        self.write_xml(name="battleship", size="4")
 
+    def post(self):
+        logger.debug("POST-PlaceShip: %s", self.request.body)
+        self.write_xml(allowed="yes")
+
+class WaitForTurnHandler(XMLHandler):
+    @tornado.gen.coroutine
+    def get(self):
+        logger.debug("WaitForTurn: %s", self.request.query)
+        yield tornado.gen.sleep(9)
+        self.write_xml(gamestate="canplay")
+
+class PutCoordHandler(XMLHandler):
+    def get(self):
+        logger.debug("PutCoord: %s", self.request.query)
+        self.write_xml(shot="hit")
+
+class LogHandler(XMLHandler):
+    def get(self):
+        logger.info("GET-LOG: %s", self.request.query)
+        self.write_xml()
+
+    def post(self):
+        logger.info("POST-LOG: %s", self.request.body)
+        self.write_xml()
 
 if __name__ == "__main__":
     args = docopt(__doc__)
     loop = tornado.ioloop.IOLoop.current()
+    logger = logging.getLogger('battleships-web')
+    logger.setLevel(logging.DEBUG)
 
     # debug=True will reload the application if a file is changed,
     # and disable the template cache, among other things
     app = tornado.web.Application([
-        (r"/dialog", HelloWorld),
-        (r"/findmatch", FindMatchHandler),
-        (r"/nextship", NextShipHandler),
+        (r"/dialog", DialogHandler),
+        (r"/waitforgame", WaitForGameHandler),
+        (r"/placeship", PlaceShipHandler),
+        (r"/waitforturn", WaitForTurnHandler),
+        (r"/putcoord", PutCoordHandler),
+        (r"/log", LogHandler),
         (r"/static/(.*)", tornado.web.StaticFileHandler, {'path': 'static'})
     ], template_path="templates", debug=True)
 
