1b3b7e1fcc083a80dadda8c5c18b8c805693134b
Andreas Gnau <Rondom@Rondom.de>
2015-03-08 16:42:53 +0100

    Commit game logic and endpoints for communication

---
+++ game.py
@@ -0,0 +1,236 @@
+import collections
+from copy import copy
+from enum import Enum
+
+Orientation = Enum('Orientation', 'horizontal vertical')
+GameState = Enum('GameState', 'won lost canPlay wait')
+ShotResult = Enum('ShotResult', 'hit miss sunk alreadyShot')
+
+
+class Ship(object):
+    def __init__(self, name, size, fields_intact=None):
+        self.name = name
+        self.size = size
+        self.fields_intact = fields_intact or size
+
+    def __repr__(self):
+        return 'Ship(%s, %s, %s)' % (self.name, self.size, self.fields_intact)
+
+
+class Game(object):
+    """ Represents the game logic  """
+
+    AVAILABLE_SHIPS = [
+        (Ship('Battleship', 4, 4), 1),
+        (Ship('Destroyer', 3, 3), 1),
+        (Ship('Submarine', 2, 2), 2),
+        # (Ship('Testship', 1, 1), 1),
+    ]
+    """ Initial list of number of ships available for each type as list of tuples (ship, count)
+    """
+
+    def __init__(self, p1_token, p2_token):
+        """
+        :param p1_token: unique token/identifier for player 1
+        :param p2_token: unique token/identifier for player 2
+        """
+        self.p1_token = p1_token
+        self.p2_token = p2_token
+
+        p1_ships = Game.generate_ships_to_place()
+        p2_ships = Game.generate_ships_to_place()
+        self.whose_turn = self.p1_token
+
+        self.opponent = {
+            p1_token: p2_token,
+            p2_token: p1_token,
+        }
+        self.ships_to_place = {
+            p1_token: collections.deque(p1_ships),
+            p2_token: collections.deque(p2_ships),
+        }
+        self.own_ships = {
+            p1_token: p1_ships,
+            p2_token: p2_ships,
+        }
+        self.grids = {
+            p1_token: Grid(),
+            p2_token: Grid(),
+        }
+
+        self.moves_done = {
+            p1_token: [],
+            p2_token: [],
+        }
+
+        self.whose_turn = self.p1_token
+
+    def get_game_state(self, player_token):
+        """
+
+        :param player_token:
+        :return:
+        """
+        all_ships_placed = not self.ships_to_place[self.p1_token] and not self.ships_to_place[self.p1_token]
+
+        state = GameState.wait
+        if not sum((ship.fields_intact for ship in self.own_ships[player_token])):
+            state = GameState.lost
+        elif not sum((ship.fields_intact for ship in self.own_ships[self.opponent[player_token]])):
+            state = GameState.won
+        elif all_ships_placed and self.is_players_turn(player_token):
+            state = GameState.canPlay
+
+        return state
+
+    def is_players_turn(self, player_token):
+        """
+        :param player_token: unique player token
+        :return: true if it is the players turn; false if it is not
+        """
+        return self.whose_turn == player_token
+
+    def get_ship_to_place(self, player_token):
+        """
+        Returns instance of the ship that is to be placed next by the given player
+        :param player_token: unique player token
+        :return: instance of Ship, None if all ships have been placed
+        """
+        if self.ships_to_place[player_token]:
+            return self.ships_to_place[player_token][0]
+        else:
+            return None
+
+
+    def place_ship(self, player_token, top_left_coord, orientation):
+        """
+        Places the *current* ship (returned by get_ship_to_place)
+        :param player_token: unique player token
+        :param top_left_coord: top left Coord of the ship on the field
+        :param orientation: orientation of the ship on the field, e.g. Orientation.vertical
+        :raises OccupiedFieldsException if fields are blocked by other ships
+        :raises IndexError if coord out of range
+        """
+
+        ship_to_place = self.get_ship_to_place(player_token)
+        grid = self.grids[player_token]
+        if orientation is Orientation.horizontal:
+            coord_range_x = range(top_left_coord.x, ship_to_place.size)
+            coords = [Coord(x, top_left_coord.y) for x in coord_range_x]
+        else:
+            coord_range_y = range(top_left_coord.y, ship_to_place.size)
+            coords = [Coord(top_left_coord.x, y) for y in coord_range_y]
+
+        grid.put(ship_to_place, coords)
+        self.ships_to_place[player_token].popleft()
+
+    def shoot_field(self, player_token, opponent_field_coord):
+        """
+
+        :param player_token:
+        :param opponent_field_coord:
+        :return: a ShotResult
+        """
+        if self.get_game_state(player_token) != GameState.canPlay:
+            raise Exception('Not your turn')
+
+        opponent = self.opponent[player_token]
+        opponent_field = self.grids[opponent]
+        what_is_at_coord = opponent_field[opponent_field_coord]
+        shot_result = None
+        if what_is_at_coord is None:
+            shot_result = ShotResult.miss
+        elif what_is_at_coord is Grid.FIELD_SHOT:
+            shot_result = ShotResult.alreadyShot
+        else:
+            shot_result = ShotResult.hit
+            what_is_at_coord.fields_intact -= 1
+            opponent_field[opponent_field_coord] = Grid.FIELD_SHOT
+            if not what_is_at_coord.fields_intact:
+                shot_result = ShotResult.sunk
+        if shot_result != ShotResult.alreadyShot:
+            self.moves_done[player_token].append((opponent_field_coord, what_is_at_coord))
+            self.whose_turn = opponent
+
+        return shot_result
+
+    def get_last_opponent_move(self, player_token):
+        opponent = self.opponent[player_token]
+        if self.moves_done[opponent]:
+            move_done = self.moves_done[opponent][-1]
+            return move_done
+        return None, None
+
+    @classmethod
+    def generate_ships_to_place(cls):
+        ships_to_place = collections.deque()
+        for (ship, number) in cls.AVAILABLE_SHIPS:
+            for _ in range(number):
+                # copying here, because we need to be able to detect that an /individual/ ship has sunk
+                ships_to_place.append(copy(ship))
+        return ships_to_place
+
+
+class Coord(object):
+    def __init__(self, x, y):
+        """
+        Accepts coordinates either as ('A', 1') or (*'A1') or (0,0) or with kwargs x and y
+        """
+
+        if isinstance(x, str):
+            self.x = ord(x) - ord('A')
+            self.y = int(y) - 1
+        else:
+            self.x = x
+            self.y = y
+
+    def __str__(self):
+        x_str = chr(self.x + ord('A'))
+        y_str = str(self.y + 1)
+        return x_str + y_str
+
+    def __repr__(self):
+        return 'Coord(*%s)' % str(self)
+
+
+class OccupiedFieldsException(Exception):
+    def __init__(self, message=None, occupied_fields=None):
+        """
+        :param message: exception message
+        :param occupied_fields: list of Coord-objects denoting the fields that are occupied
+        :return:
+        """
+        super(Exception, self).__init__(message)
+        self.occupied_fields = occupied_fields
+
+
+class Grid(object):
+    GRID_SIZE = 6
+
+    FIELD_SHOT = "SHOT"
+
+    def __init__(self):
+        self.field = [[None for _ in range(self.GRID_SIZE)] for i in range(self.GRID_SIZE)]
+
+    def __getitem__(self, coord):
+        return self.field[coord.x][coord.y]
+
+    def __setitem__(self, coord, value):
+        self.field[coord.x][coord.y] = value
+
+    def put(self, ship, coords):
+        """
+        :param ship: instance of Ship (note to properly clone objects, when placing two ships of the same type)
+        :param coords: list of Coords
+        :raises IndexError if ship is partially or completely out of grid
+        :raises OccupiedFieldsException if field is not empty
+        :return: None
+        """
+
+        occupied_fields = [coord for coord in coords if self[coord] is not None]
+        if occupied_fields:
+            raise OccupiedFieldsException(occupied_fields=occupied_fields)
+
+        for coord in coords:
+            self[coord] = ship
+        print('Put %s at %s' % (str(ship), coords))
--- main.py
+++ main.py
@@ -10,15 +10,54 @@ Options:
   -l ADDR, --listen=ADDR    Address to listen on, by default all interfaces
 """
 
-import datetime
 import logging
 import uuid
+import time
 
 from docopt import docopt
 from lxml import etree
 import tornado.ioloop
 import tornado.gen
 import tornado.web
+
+import game
+
+
+class SessionTokenToGame(object):
+    """
+    Usage:
+    >>> g = SessionTokenToGame()
+    >>> g[42]
+    None
+    >>> g[123]
+    Game('42', 123)
+    >>> g[42]
+    Game('42', 123)
+    """
+
+    def __init__(self):
+        self.session_token_game_dict = {}
+        self.unmatched_players = set()
+
+    def __getitem__(self, player_token):
+        if player_token not in self.session_token_game_dict:
+            logger.debug('new token')
+            self.session_token_game_dict[player_token] = None
+            self.unmatched_players.add(player_token)
+
+        if self.session_token_game_dict[player_token] is None and len(self.unmatched_players) > 1:
+            unmatched_player = self.unmatched_players.pop()
+            if unmatched_player == player_token:  # oops, we popped ourselves, let's pop once more
+                unmatched_player = self.unmatched_players.pop()
+            logger.debug('matrching players')
+            self.unmatched_players.discard(player_token)
+
+            new_game = game.Game(unmatched_player, player_token)
+            self.session_token_game_dict[unmatched_player] = new_game
+            self.session_token_game_dict[player_token] = new_game
+
+        return self.session_token_game_dict[player_token]
+
 
 class XMLHandler(tornado.web.RequestHandler):
     # called before running any other methods (get, post, etc)
@@ -30,6 +69,7 @@ class XMLHandler(tornado.web.RequestHandler):
         logger.debug("Response: %s", response)
         self.write(response)
 
+
 # new instances of the request handlers are created on every request,
 # so changes to member variables won't be seen across requests
 class DialogHandler(XMLHandler):
@@ -38,33 +78,107 @@ class DialogHandler(XMLHandler):
         self.render("dialog.xml", token=token)
 
 
-class WaitForGameHandler(XMLHandler):
+class DynamicDataHandler(XMLHandler):
+    TIMEOUT_SECONDS = 9.0
+    POLL_INTERVAL_SECONDS = 0.5
+
+    def prepare(self):
+        self.token = self.get_argument('token')
+        self.game = GAMES[self.token]
+        self.out = {}
+
+    def check_with_timeout(self, what, timeout_value=None):
+        start_time = time.time()
+        result = None
+        while True:
+            result = what()
+            if result != timeout_value:
+                return result
+            elif time.time() - start_time > self.TIMEOUT_SECONDS:
+                return timeout_value
+            yield tornado.gen.sleep(self.POLL_INTERVAL_SECONDS)
+
+
+class WaitForGameHandler(DynamicDataHandler):
     @tornado.gen.coroutine
     def get(self):
         logger.debug("WaitForGame: %s", self.request.query)
-        yield tornado.gen.sleep(9)
-        self.write_xml(ready="true")
+        ready = yield from self.check_with_timeout(lambda: GAMES[self.token], None)
+        self.out['ready'] = str(bool(ready)).lower()
+        self.write_xml(**self.out)
 
-class PlaceShipHandler(XMLHandler):
+
+class PlaceShipHandler(DynamicDataHandler):
+    def prepare(self):
+        super(PlaceShipHandler, self).prepare()
+        if not self.game:
+            raise Exception('no_game')
+
+
     def get(self):
         logger.debug("GET-PlaceShip: %s", self.request.query)
-        self.write_xml(name="battleship", size="4")
+        self._add_next_ship_info_to_output()
+        self.write_xml(**self.out)
+
+    def _add_next_ship_info_to_output(self):
+        ship = self.game.get_ship_to_place(self.token)
+        if ship:
+            self.out.update({
+                'name': ship.name,
+                'size': str(ship.size),
+            })
+
 
     def post(self):
         logger.debug("POST-PlaceShip: %s", self.request.body)
-        self.write_xml(allowed="yes")
+        self.out.update({
+            'conflictingcoords': '',
+            'allowed': 'yes',
+        })
 
-class WaitForTurnHandler(XMLHandler):
+        coord = game.Coord(*self.get_argument('coord'))
+        map_orientation = {
+            'horizontally': game.Orientation.horizontal,
+            'vertically': game.Orientation.vertical,
+        }
+
+        orientation = map_orientation[self.get_argument('orientation')]
+        try:
+            self.game.place_ship(self.token, top_left_coord=coord, orientation=orientation)
+        except game.OccupiedFieldsException as e:
+            self.out['conflictingcoords'] = ' '.join(str(occ) for occ in e.occupied_fields)
+            self.out['allowed'] = 'conflict'
+        except IndexError:
+            self.out['allowed'] = 'beyondfield'
+        self._add_next_ship_info_to_output()
+        self.write_xml(**self.out)
+
+
+class WaitForTurnHandler(DynamicDataHandler):
     @tornado.gen.coroutine
     def get(self):
         logger.debug("WaitForTurn: %s", self.request.query)
-        yield tornado.gen.sleep(9)
-        self.write_xml(gamestate="canplay")
+        game_state = yield from self.check_with_timeout(lambda: self.game.get_game_state(self.token),
+                                                        timeout_value=game.GameState.wait)
+        self.out['gamestate'] = game_state.name
+        if game_state == game.GameState.canPlay:
+            (coord, what_was_at_coord) = self.game.get_last_opponent_move(self.token)
+            if coord:
+                self.out['coordhit'] = str(coord)
+            if what_was_at_coord:
+                self.out['shiptypehit'] = what_was_at_coord.name
+                self.out['shippartsleft'] = str(what_was_at_coord.fields_intact)
+        self.write_xml(**self.out)
 
-class PutCoordHandler(XMLHandler):
+
+class PutCoordHandler(DynamicDataHandler):
     def get(self):
         logger.debug("PutCoord: %s", self.request.query)
-        self.write_xml(shot="hit")
+        coord = game.Coord(*self.get_argument('coord'))
+        shot_result = self.game.shoot_field(self.token, coord)
+        self.out['shot'] = shot_result.name
+        self.write_xml(**self.out)
+
 
 class LogHandler(XMLHandler):
     def get(self):
@@ -74,6 +188,7 @@ class LogHandler(XMLHandler):
     def post(self):
         logger.info("POST-LOG: %s", self.request.body)
         self.write_xml()
+
 
 if __name__ == "__main__":
     args = docopt(__doc__)
@@ -85,18 +200,20 @@ if __name__ == "__main__":
     # debug=True will reload the application if a file is changed,
     # and disable the template cache, among other things
     app = tornado.web.Application([
-        (r"/dialog", DialogHandler),
-        (r"/waitforgame", WaitForGameHandler),
-        (r"/placeship", PlaceShipHandler),
-        (r"/waitforturn", WaitForTurnHandler),
-        (r"/putcoord", PutCoordHandler),
-        (r"/log", LogHandler),
-        (r"/static/(.*)", tornado.web.StaticFileHandler, {'path': 'static'})
-    ], template_path="templates", debug=True)
+                                      (r"/dialog", DialogHandler),
+                                      (r"/waitforgame", WaitForGameHandler),
+                                      (r"/placeship", PlaceShipHandler),
+                                      (r"/waitforturn", WaitForTurnHandler),
+                                      (r"/putcoord", PutCoordHandler),
+                                      (r"/log", LogHandler),
+                                      (r"/static/(.*)", tornado.web.StaticFileHandler, {'path': 'static'})
+                                  ], template_path="templates", debug=True)
 
     port = int(args['--port'])
     address = args['--listen'] or ''
     app.listen(port, address=address)
+
+    GAMES = SessionTokenToGame()
 
     # this starts the event loop. Tornado is single threaded,
     # and will sleep until it is notified of new events on one of
